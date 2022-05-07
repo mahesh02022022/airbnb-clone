d26b9a104bb25c4212c5b253119c13cd3a8665e0
mahesh02022022 <mahesh02022022+github@gmail.com>
2022-05-07 20:21:34 +0530

    Initial commit

---
+++ precache-manifest.9213c2e83ce5ded9530c19ab94f9be17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd1677b47499c295aa7062729d8cb0b",
+    "url": "/airbnb-clone/index.html"
+  },
+  {
+    "revision": "9a07403f13520f20e23f",
+    "url": "/airbnb-clone/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "1e5f2f821b0522bb9d17",
+    "url": "/airbnb-clone/static/css/main.4bcf68ee.chunk.css"
+  },
+  {
+    "revision": "9a07403f13520f20e23f",
+    "url": "/airbnb-clone/static/js/2.7440f58a.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/airbnb-clone/static/js/2.7440f58a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e5f2f821b0522bb9d17",
+    "url": "/airbnb-clone/static/js/main.dcd4d7c5.chunk.js"
+  },
+  {
+    "revision": "f358a9c4d5787a3a04f2",
+    "url": "/airbnb-clone/static/js/runtime-main.fc586be8.js"
+  }
+]);
